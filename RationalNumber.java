b9af22fd6a10de5d7dc51202a869c4bd07fd36cc
Sarah Leong <sleong20@stuy.edu>
2020-11-21 13:04:41 -0500

    added and tested toString

--- RationalNumber.java
+++ RationalNumber.java
@@ -26,6 +26,9 @@ public class RationalNumber extends RealNumber {
     RationalNumber e = new RationalNumber(1, 3);
     System.out.println(a.equals(c));
     System.out.println(a.equals(e));
+
+    // testing toString
+    System.out.println(a.toString());
   }
 
   /**Initialize the RationalNumber with the provided values
@@ -83,7 +86,7 @@ public class RationalNumber extends RealNumber {
   *@return the value expressed as "3/4" or "8/3"
   */
   public String toString(){
-    return "0";
+    return numerator+"/"+denominator;
   }
 
   /**Calculate the GCD of two integers.
