d44e17b75c8b3c66d39dca5dd43f4a1ac3f017b7
Sarah Leong <sleong20@stuy.edu>
2020-11-27 13:05:42 -0500

    added and tested compareTo

--- Number.java
+++ Number.java
@@ -1,4 +1,16 @@
 public abstract class Number{
+  public static void main(String[] args) {
+    Number a = new RealNumber(1.00231);
+    Number b = new RealNumber(1.00232);
+    Number c = new RealNumber(0.00001);
+    Number d = new RationalNumber(9,0);
+    Number f = new RealNumber(0);
+    
+    // testing compareTo
+    System.out.println(d.compareTo(f));
+    System.out.println(a.compareTo(b));
+    System.out.println(b.compareTo(a));
+  }
   public abstract double getValue();
 
   /*return 0 when this Number equals the other RealNumber
@@ -6,8 +18,12 @@ public abstract class Number{
   return a positive value when this Number is larger than the other Number
   */
   public int compareTo(Number other){
-    //TO BE IMPLEMENTED
-    return 0;
+    if (this.getValue()==other.getValue()) {
+        return 0;
+    } else if (this.getValue() < other.getValue()) {
+        return -1;
+    }
+    return 1;
   }
 
   /*
